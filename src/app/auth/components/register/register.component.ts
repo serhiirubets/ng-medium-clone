3a83c12f326ceda24f8d96e6d185cb73524d1dac
serhii.rubets <serhii.rubets@playtika.com>
2020-07-10 10:59:02 +0300

    Added disabled to register button

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Store} from '@ngrx/store';
+import {select, Store} from '@ngrx/store';
 import {registerAction} from '../../store/actions/register.action';
+import {Observable} from 'rxjs';
+import {isSubmittingSelector} from '../../store/selectors';
 
 @Component({
   selector: 'mc-register',
@@ -10,12 +12,14 @@ import {registerAction} from '../../store/actions/register.action';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
 
   constructor(private fb: FormBuilder, private store: Store) {
   }
 
   ngOnInit(): void {
-    this.initializeForm()
+    this.initializeForm();
+    this.initializeValues();
   }
 
 
@@ -30,5 +34,10 @@ export class RegisterComponent implements OnInit {
   onSubmit(): void {
     console.log(this.form.valid);
     this.store.dispatch(registerAction(this.form.value))
+  }
+
+  private initializeValues(): void {
+    this.isSubmitting$ = this.store
+      .pipe(select(isSubmittingSelector));
   }
 }
---
+++ src/app/auth/store/selectors.ts
@@ -0,0 +1,7 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {AppStateInterface} from '../../shared/types/appState.interface';
+import {AuthStateInterface} from '../types/authState.interface';
+
+export const authFeatureSelector = createFeatureSelector<AppStateInterface, AuthStateInterface>('auth');
+
+export const isSubmittingSelector = createSelector(authFeatureSelector, (auth: AuthStateInterface) => auth.isSubmitting);
