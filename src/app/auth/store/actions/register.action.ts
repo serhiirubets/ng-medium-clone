16ed120a7e87686db9967e884aa5f4bd3f9d2cf0
serhii.rubets <serhii.rubets@playtika.com>
2020-07-10 10:36:58 +0300

    Added auth reducer

--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -1,7 +1,8 @@
 import {createAction, props} from '@ngrx/store';
 import {ActionTypes} from '../action.types';
+import {RegisterRequestInterface} from '../../types/registerRequest.interface';
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<{username: string, password: string, email: string}>()
+  props<RegisterRequestInterface>()
 );
---
+++ src/app/shared/types/currentUser.interface.ts
@@ -0,0 +1,10 @@
+export interface CurrentUserInterface {
+  id: number;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  username: string;
+  bio: null | string;
+  image: null | string;
+  token: string;
+}
